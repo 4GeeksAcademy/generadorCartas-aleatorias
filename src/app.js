763cfa6125cd7d955c1da79b47605d5f9df29e86
CristhianMz98 <cmtz2503@gmail.com>
2025-05-09 19:57:48 +0000

    generador de cartas

--- src/app.js
+++ src/app.js
@@ -1,11 +1,24 @@
-import "bootstrap";
-import "./style.css";
-
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+window.onload = function () {
+  generateRandomCard();
 };
+
+function generateRandomCard() {
+  const values = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K'];
+
+  const suits = [
+    { symbol: '♥', name: 'heart' },
+    { symbol: '♠', name: 'spade' },
+    { symbol: '♣', name: 'club' },
+    { symbol: '♦', name: 'diamond' }
+  ];
+
+  const value = values[Math.floor(Math.random() * values.length)];
+  const suit = suits[Math.floor(Math.random() * suits.length)];
+
+  
+  document.querySelector('.top-suit').textContent = suit.symbol;
+  document.querySelector('.bottom-suit').textContent = suit.symbol;
+  document.querySelector('.card-value').textContent = value;
+
+  document.querySelector('.card').className = 'card mx-auto mt-5 ' + suit.name;
+}
