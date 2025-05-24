8c986478ab9bf38a439c6c0dd263b44648a8a710
CristhianMz98 <cmtz2503@gmail.com>
2025-05-24 05:09:19 +0000

    renombradas variables con nombres mas descriptivos

--- src/app.js
+++ src/app.js
@@ -3,22 +3,21 @@ window.onload = function () {
 };
 
 function generateRandomCard() {
-  const values = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K'];
+  const cardValues = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K'];
 
-  const suits = [
+  const cardSuits = [
     { symbol: '♥', name: 'heart' },
     { symbol: '♠', name: 'spade' },
     { symbol: '♣', name: 'club' },
     { symbol: '♦', name: 'diamond' }
   ];
 
-  const value = values[Math.floor(Math.random() * values.length)];
-  const suit = suits[Math.floor(Math.random() * suits.length)];
+  const selectedValue = cardValues[Math.floor(Math.random() * cardValues.length)];
+  const selectedSuit = cardSuits[Math.floor(Math.random() * cardSuits.length)];
 
-  
-  document.querySelector('.top-suit').textContent = suit.symbol;
-  document.querySelector('.bottom-suit').textContent = suit.symbol;
-  document.querySelector('.card-value').textContent = value;
+  document.querySelector('.top-suit').textContent = selectedSuit.symbol;
+  document.querySelector('.bottom-suit').textContent = selectedSuit.symbol;
+  document.querySelector('.card-value').textContent = selectedValue;
 
-  document.querySelector('.card').className = 'card mx-auto mt-5 ' + suit.name;
+  document.querySelector('.card').className = 'card mx-auto mt-5 ' + selectedSuit.name;
 }
